91b61b6c51d35507ca9e16c53b43e9c8f1909db1
ahmed sobhy <eng_ahmed9393@yahoo.com>
2016-04-05 22:40:50 +0200

    m

--- appClinic/forms.py
+++ appClinic/forms.py
@@ -5,6 +5,7 @@ from django.forms import ModelForm
 from django.core.exceptions import NON_FIELD_ERRORS
 from django.contrib.admin.widgets import AdminDateWidget
 from django.contrib.admin import widgets        
+from django.contrib.admin.widgets import AdminDateWidget
 
 class nameForm(forms.Form):
 	yourName=forms.CharField(label="Your name",max_length=20)
@@ -25,15 +26,18 @@ class LabForm(ModelForm):
 			'unique_together': "%(model_name)s's %(field_labels)s are not unique.",
 			}
 		}
-		
+		widgets = {
+           		'wtf': AdminDateWidget(),
+           		'wtt': AdminDateWidget(),
+       		}	
 	#
 	def clean(self):
 		pass
 
 	def __init__(self, *args, **kwargs):
 		super(LabForm, self).__init__(*args, **kwargs)
-		self.fields['wtf'].widget = widgets.AdminSplitDateTime()
-		self.fields['wtt'].widget = widgets.AdminSplitDateTime()
+		# self.fields['wtf'].widget = widgets.AdminSplitDate()
+		# self.fields['wtt'].widget = widgets.AdminSplitDate()
 		
 		
 		
--- appClinic/views.py
+++ appClinic/views.py
@@ -36,8 +36,10 @@ def yourName(request):
 	return HttpResponse("your name is "+request.POST.get("yourName", ""))
 
 
+
 def addlap(request):
 	if(request.method=='POST'):
+
 		labForm = LabForm(request.POST)
 
 		if(labForm.is_valid):
@@ -54,34 +56,31 @@ def addlap(request):
 
 
 def lapName(request):
- #I'm the a8baaaa wa7d f el doniaaaaa 
 
- #Stupid ...........................Stupid
 
-	#done added lap
 	if(request.method=='POST'):
 
 			labForm = LabForm(request.POST)
 			if labForm.is_valid():
 				wtt=request.POST.get('wtt','')
-				wtt=wtt.replace('+',' ')
+				# wtt=wtt.replace('+',' ')
 
 				wtf=request.POST.get('wtf','')
-				wtf=wtf.replace('+',' ')
+				# wtf=wtf.replace('+',' ')
 
 				lab=labForm.save(commit=False)
-				user=myuser.objects.get(pk=2)
-				
-				lab.wtf=wtf
-				lab.wtt=wtt
-				lab.owner=request.user
-				
+				# user=myuser.objects.get(request.user.id)
+				# user=myuser(pk=request.user.id)
+				# user.save()
+				# lab.wtf=wtf
+				# lab.wtt=wtt
+				# lab.owner=myuser.objects.get(request.user)
+				lab.owner=myuser.objects.get(pk=1)
 				lab.save()
 			else:
 				return HttpResponseRedirect('/appClinic/invalidForm.html')
 
 	return HttpResponse("Lap Name "+request.POST.get("name",""))
-
 def invalidForm(request):
 	return HttpResponse("Invalid Form >>> ")
 
