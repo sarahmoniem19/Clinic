dcc1a61cc694dc0d63d1b5e5d73a1bd9e6e56696
Zahra Meki <zahrameki18@gmail.com>
2016-04-05 14:41:26 +0200

    1st

--- ClinicProject/settings.py
+++ ClinicProject/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'ClinicProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
